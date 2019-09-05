a5989eb0275235995b00db5807ea774ccd0a89ba
Nipul Singal <42814830+NipulSingal@users.noreply.github.com>
2019-09-05 15:14:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+var inquirer = require("inquirer");
+var chalk = require("chalk");
+
+const title = chalk.bold.green;
+const response = chalk.blue;
+
+var resume = require("./helpers/resume.json");
+var dividerConstructor = require("./helpers/divider.js");
+var divider = new dividerConstructor(70, "white");
+
+var resumePrompts = {
+  type: "list",
+  name: "resumeOptions",
+  message: "What do you want to know about me?",
+  choices: [...Object.keys(resume), "Exit"]
+};
+
+function resumeHandler() {
+  console.log("Hello,My name is Nipul Singal and welcome to my resume");
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == "Exit") {
+      return;
+    }
+    var option = answer.resumeOptions;
+
+    divider.printTop();
+
+    resume[`${option}`].forEach((info, ind) => {
+      if (typeof info === "string") {
+        console.log(divider.containString(`  ${info}`, response));
+      } else {
+        Object.values(info).forEach((value, ind) => {
+          if (ind > 1) {
+            console.log(divider.containString(`    ${value}`, response));
+          } else if (ind == 1) {
+            console.log(divider.containString(`    ${value}`, chalk.cyan.bold));
+          } else {
+            console.log(divider.containString(`  ${value.padEnd(38)}`, title));
+          }
+        });
+        ind !== resume[`${option}`].length - 1 && divider.printLine();
+      }
+    });
+    divider.printBottom();
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.exitBack == "Back") {
+          process.stdout.write("\033c");
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+}
+
+(() => {
+  console.clear();
+  resumeHandler();
+})();
